bbbfb62c4d28d1bc8458f8da5ea5565c3638d5a6
stanislavermohin <ermohin1999@gmail.com>
2025-03-19 15:46:35 +0300

    Bug fix

--- test_app_cicd.py
+++ test_app_cicd.py
@@ -13,14 +13,16 @@ class TestLLM:
     Test LLM API predictions (5/5 passed expected, however at least 1/5 is fine)
     """
 
+    PREDICTION_APP: PredictionApp = PredictionApp()
+
 
     def test_any(self):
         """
         Run abstract LLM prediction on test data (check if not None)
         :return:
         """
-        prediction_app = PredictionApp()
-        prediction_function = prediction_app.predict_with_any_llm
+
+        prediction_function = self.PREDICTION_APP.predict_with_any_llm
 
         assert prediction_function(TestData.DEFAULT_DATA_TO_TEST_API_UP) is not None, \
             "Couldn't get prediction"
@@ -30,8 +32,8 @@ class TestLLM:
         Run LLM prediction with prob. on test data
         :return:
         """
-        prediction_app = PredictionApp()
-        prediction_function = prediction_app.predict_probability_with_llm
+
+        prediction_function = self.PREDICTION_APP.predict_probability_with_llm
 
         assert prediction_function(TestData.DEFAULT_DATA_TO_TEST_API_UP) == "up", \
             "Incorrect prediction"
@@ -41,22 +43,22 @@ class TestLLM:
         Run LLM prediction with prob. on test data
         :return:
         """
-        prediction_app = PredictionApp()
-        prediction_function = prediction_app.predict_probability_with_llm
+
+        prediction_function = self.PREDICTION_APP.predict_probability_with_llm
 
         assert prediction_function(TestData.DEFAULT_DATA_TO_TEST_API_DOWN) == "down", \
             "Incorrect prediction"
 
     def test_basic_up(self):
-        prediction_app = PredictionApp()
-        prediction_function = prediction_app.predict_up_or_down
+
+        prediction_function = self.PREDICTION_APP.predict_up_or_down
 
         assert prediction_function(TestData.DEFAULT_DATA_TO_TEST_API_UP) == "up", \
             "Incorrect prediction"
 
     def test_basic_down(self):
-        prediction_app = PredictionApp()
-        prediction_function = prediction_app.predict_up_or_down
+
+        prediction_function = self.PREDICTION_APP.predict_up_or_down
 
         assert prediction_function(TestData.DEFAULT_DATA_TO_TEST_API_DOWN) == "down", \
             "Incorrect prediction"
