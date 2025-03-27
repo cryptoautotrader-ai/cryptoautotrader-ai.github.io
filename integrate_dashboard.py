8b12013f833004086886d425eb7ec93fda646639
Stan <ermohin1999@gmail.com>
2025-03-27 14:24:08 +0300

    Comments and doc strings in integrate_dashboard.py

--- integrate_dashboard.py
+++ integrate_dashboard.py
@@ -4,18 +4,33 @@ from dashboard import add_info_message, add_memory_messages, add_transaction_cos
 
 
 class OutputIntegration:
+    """Class to connect messages logic to other modules"""
+
     def __init__(self: Self, mode: Literal["dashboard", "console"]):
         self.mode = mode
 
     @property
     def output(self: Self):
+        """How to process a general info message
+
+        :return: Callable object
+        """
+
+        # Just printing the message out
         if self.mode == "console":
             return print
+
+        # Running the message through another function
         elif self.mode == "dashboard":
             return add_info_message
 
     @property
     def handle_data(self: Self):
+        """How to process deal value (cost of transaction)
+
+        :return: Callable object
+        """
+
         # Doing nothing when data is received by this function
         if self.mode == "console":
             return lambda *args, **kwargs: None
@@ -25,11 +40,16 @@ class OutputIntegration:
             return add_transaction_cost
 
     @property
-    def handle_memory_data(
-            self: Self
-    ):
+    def handle_memory_data(self: Self):
+        """How to process a memory message
+
+        :return: Callable object
+        """
+
+        # Just printing the message out
         if self.mode == "console":
             return print
 
+        # Running the message through another function
         elif self.mode == "dashboard":
             return add_memory_messages
